a1a7ffcba675e7e69ff51ead635a940e37f49b87
Angus Morrison <contact@angus-morrison.com>
2020-07-08 07:46:23 +0100

    Refactored to use t.Run

--- url_shortener/from_db/urlshort/db_test.go
+++ url_shortener/from_db/urlshort/db_test.go
@@ -1,6 +1,8 @@
 package urlshort
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
 
@@ -11,81 +13,88 @@ const seedFixture = "fixtures/seeds_test.yaml"
 
 func TestOpenRedirectStore(t *testing.T) {
 	oldDBName := dbName
-	dbName = "test.db"
+	dbName = tempfile()
+	defer os.Remove(dbName)
 
-	// Test RedirectStore is created
+	// Test DB creation in main routine to ensure failure of test suite and proper teardown if it
+	// fails.
 	store, err := OpenRedirectStore()
 	if err != nil {
-		t.Errorf("opening DB: %v", err)
+		t.Fatalf("opening DB: %v", err)
 	}
+	defer store.DB.Close()
 
-	// Test bucket is created
-	bucketExists := false
-	store.DB.View(func(tx *bolt.Tx) error {
-		if b := tx.Bucket([]byte(bucketName)); b != nil {
-			bucketExists = true
+	t.Run(fmt.Sprintf("creates bucket %q", bucketName), func(t *testing.T) {
+		bucketExists := false
+		store.DB.View(func(tx *bolt.Tx) error {
+			if b := tx.Bucket([]byte(bucketName)); b != nil {
+				bucketExists = true
+			}
+			return nil
+		})
+		if !bucketExists {
+			t.Fatalf("default bucket %q was not created", bucketName)
 		}
-		return nil
 	})
-	if !bucketExists {
-		t.Errorf("default bucket %q was not created", bucketName)
-		store.DB.Close()
-	}
 
-	// Clean up
-	store.DB.Close()
-	os.Remove(dbName)
 	dbName = oldDBName
 }
 
 func TestSeed(t *testing.T) {
 	oldDBName := dbName
-	dbName = "test.db"
+	dbName = tempfile()
+	defer os.Remove(dbName)
+
 	store, err := OpenRedirectStore()
 	if err != nil {
-		t.Errorf("failed to create test DB: %v", err)
+		panic(err)
 	}
 	defer store.DB.Close()
 
 	// Test for errors
-	if err := store.Seed(seedFixture); err != nil {
-		t.Errorf("seeding DB: %v", err)
-	}
+	t.Run("seeds the DB without errors", func(t *testing.T) {
+		if err := store.Seed(seedFixture); err != nil {
+			t.Fatalf("seeding DB: %v", err)
+		}
+	})
 
-	// Test key count
 	redirects, _ := readSeedFile(seedFixture) // ignore error; must succeed if Seed succeeded
-	store.DB.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(bucketName))
-		keyN := b.Stats().KeyN
-		if keyN != len(redirects) {
-			t.Errorf("found %d entries in the DB, want %d", keyN, len(redirects))
-		}
-		return nil
+	t.Run("adds the correct number of rows to the DB", func(t *testing.T) {
+		store.DB.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte(bucketName))
+			keyN := b.Stats().KeyN
+			if keyN != len(redirects) {
+				t.Fatalf("found %d entries in the DB, want %d", keyN, len(redirects))
+			}
+			return nil
+		})
 	})
 
-	// Test key-value pair integrity
-	r := redirects[0]
-	store.DB.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(bucketName))
-		v := b.Get([]byte(r.Path))
-		if gotURL := string(v); gotURL != r.URL {
-			t.Errorf("entry for path %q had URL %q, want %q", r.Path, gotURL, r.URL)
-		}
-		return nil
+	t.Run("adds the correct key-value pairs to the DB", func(t *testing.T) {
+		r := redirects[0]
+		store.DB.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte(bucketName))
+			v := b.Get([]byte(r.Path))
+			if gotURL := string(v); gotURL != r.URL {
+				t.Fatalf("entry for path %q had URL %q, want %q", r.Path, gotURL, r.URL)
+			}
+			return nil
+		})
 	})
 
-	// Test false negatives
-	invalidPath := "/invalid"
-	store.DB.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(bucketName))
-		v := b.Get([]byte(invalidPath))
-		if v != nil {
-			t.Errorf("unexpected URL %q for invalid path %q, want nil", v, invalidPath)
-		}
-		return nil
-	})
-
-	store.DB.Close()
-	os.Remove(dbName)
 	dbName = oldDBName
+}
+
+func tempfile() string {
+	f, err := ioutil.TempFile("", "bolt-")
+	if err != nil {
+		panic(err)
+	}
+	if err := f.Close(); err != nil {
+		panic(err)
+	}
+	if err := os.Remove(f.Name()); err != nil {
+		panic(err)
+	}
+	return f.Name()
 }
