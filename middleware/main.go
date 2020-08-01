12add2a8aa088f6d961de417807d8f5b405056b9
Angus Morrison <contact@angus-morrison.com>
2020-08-01 17:18:05 +0100

    Complete middleware basic implementation

---
+++ middleware/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"runtime/debug"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/panic", panicDemo)
+	mux.HandleFunc("/panic-after/", panicAfterDemo)
+	mux.HandleFunc("/", hello)
+	log.Fatal(http.ListenAndServe(":3000", recoverMw(mux, true)))
+}
+
+func recoverMw(app http.Handler, dev bool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Println(err)
+				stack := debug.Stack()
+				log.Println(string(stack))
+				if dev {
+					w.WriteHeader(http.StatusInternalServerError)
+					fmt.Fprintf(w, "<h1>panic: %v</h1><pre>%s</pre>", err, string(stack))
+					return
+				}
+				http.Error(w, "Something went wrong.", http.StatusInternalServerError)
+			}
+		}()
+
+		nw := &responseWriter{ResponseWriter: w}
+		app.ServeHTTP(nw, r)
+		nw.flush()
+	}
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	writes [][]byte
+	status int
+}
+
+func (rw *responseWriter) Write(b []byte) (int, error) {
+	rw.writes = append(rw.writes, b)
+	return len(b), nil
+}
+
+func (rw *responseWriter) WriteHeader(statusCode int) {
+	if statusCode < 100 || statusCode >= 600 {
+		panic("Invalid status code")
+	}
+	rw.status = statusCode
+}
+
+func (rw *responseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	hijacker, ok := rw.ResponseWriter.(http.Hijacker)
+	if !ok {
+		return nil, nil, fmt.Errorf("Hijack called for ResponseWriter that doesn't implement Hijacker")
+	}
+	return hijacker.Hijack()
+}
+
+func (rw *responseWriter) Flush() {
+	flusher, ok := rw.ResponseWriter.(http.Flusher)
+	if !ok {
+		log.Printf("Flush called for ResponseWriter that doesn't implement Flusher")
+		return
+	}
+	flusher.Flush()
+}
+
+func (rw *responseWriter) flush() error {
+	if rw.status != 0 {
+		rw.ResponseWriter.WriteHeader(rw.status)
+	}
+
+	for _, write := range rw.writes {
+		_, err := rw.ResponseWriter.Write(write)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func panicDemo(w http.ResponseWriter, r *http.Request) {
+	funcThatPanics()
+}
+
+func panicAfterDemo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello!</h1>")
+	funcThatPanics()
+}
+
+func funcThatPanics() {
+	panic("Oh no!")
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello!</h1>")
+}
